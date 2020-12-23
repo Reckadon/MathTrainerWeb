a800691823f9d803bc648b0a0f3e1b70831d1978
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2020-12-23 17:40:53 +0530

    first

---
+++ src/components/diffSelector.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class DiffSelector extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Diff selector</h1>
+      </div>
+    );
+  }
+}
+
+export default DiffSelector;
