b330d370a7b2a35fa0266684c6b14c6d5ca84bc3
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2020-12-23 21:28:15 +0530

    done

--- src/components/diffSelector.js
+++ src/components/diffSelector.js
@@ -1,32 +1,29 @@
 import React, { Component } from "react";
 import "./diffSelector.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class DiffSelector extends Component {
   render() {
     return (
       <div className="buttonsGrp">
-        <Router>
-          <h3 id="selectDiffLabel">Select Difficulty:</h3>
-          <button
-            className="diffButton"
-            onClick={() => this.props.onDiffSelected("Easy")}
-          >
-            Easy
-          </button>
-          <button
-            className="diffButton"
-            onClick={() => this.props.onDiffSelected("Medium")}
-          >
-            Medium
-          </button>
-          <button
-            className="diffButton"
-            onClick={() => this.props.onDiffSelected("Hard")}
-          >
-            Hard
-          </button>
-        </Router>
+        <h3 id="selectDiffLabel">Select Difficulty:</h3>
+        <button
+          className="diffButton"
+          onClick={() => this.props.onDiffSelected("Easy")}
+        >
+          Easy
+        </button>
+        <button
+          className="diffButton"
+          onClick={() => this.props.onDiffSelected("Medium")}
+        >
+          Medium
+        </button>
+        <button
+          className="diffButton"
+          onClick={() => this.props.onDiffSelected("Hard")}
+        >
+          Hard
+        </button>
       </div>
     );
   }
