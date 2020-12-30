79db8d0c39d784123b4b93e0786d236cf917ec77
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2020-12-30 21:22:44 +0530

    difficulties working

--- src/components/card.js
+++ src/components/card.js
@@ -63,30 +63,93 @@ class Card extends Component {
     if (!this.props.isStarted) {
       return;
     }
-    const x = this.getNum();
-    const y = this.getNum();
-    const question = x + "+" + y;
+
+    const diff = this.props.diff;
+
+    let op; //operator
+    if (diff === "Easy") op = Math.random() * 10 > 5 ? " + " : " - ";
+    else {
+      if (Math.random() * 10 < 3) op = " x ";
+      else op = Math.random() * 10 > 5 ? " + " : " - ";
+    }
+
+    let multiplier; //numbers
+    if (op === " + " || op === " - ") {
+      switch (diff) {
+        case "Easy":
+          multiplier = 100;
+          break;
+        case "Medium":
+        case "Hard":
+          multiplier = 1000;
+          break;
+      }
+    } else {
+      multiplier = 100;
+    }
+    const x = this.getNum(multiplier);
+    const y = this.getNum(multiplier);
+
+    let question; //question
+    switch (op) {
+      case " + ":
+        question = x + " + " + y;
+        break;
+      case " - ":
+        if (x > y) question = x + " - " + y;
+        else question = y + " - " + x;
+        break;
+      case " x ":
+        question = x + " x " + y;
+        break;
+    }
     this.setState({ question });
-    const answer = x + y;
+    let answer; //answer
+    switch (op) {
+      case " + ":
+        answer = x + y;
+        break;
+      case " - ":
+        if (x > y) answer = x - y;
+        else answer = y - x;
+        break;
+      case " x ":
+        answer = x * y;
+        break;
+    }
     this.setState({ answer });
   }
 
   submit() {
     if (
       this.state.running &&
-      Number(this.state.entered) === this.state.answer
+      Number(this.state.entered) === this.state.answer //correct answer
     ) {
+      let points;
+      switch (
+        this.props.diff //points determination
+      ) {
+        case "Easy":
+          points = 10;
+          break;
+        case "Medium":
+          points = 20;
+          break;
+        case "Hard":
+          points = 30;
+          break;
+      }
       this.generateQuestion();
-      this.setState({ points: this.state.points + 10 });
+      this.setState({ points: this.state.points + points });
       document.getElementById("input").placeholder = "";
     } else if (
       this.state.running &&
-      (this.state.entered === "" || this.state.entered === null)
+      (this.state.entered === "" || this.state.entered === null) //no answer
     ) {
       document.getElementById("input").placeholder = "Enter a Number!";
     } else if (
       this.state.running &&
-      (this.state.entered !== "" || this.state.entered !== null)
+      (this.state.entered !== "" || this.state.entered !== null) //wrong answer
     ) {
       document.getElementById("input").placeholder = "Wrong!";
     }
@@ -94,8 +157,29 @@ class Card extends Component {
     this.setState({ entered: "" });
   }
 
-  getNum() {
-    return Math.floor(Math.random() * 100) + 1;
+  getNum(multiplier) {
+    let num;
+    if (multiplier === 100 && this.props.diff === "Medium") {
+      //for multiplication
+      do {
+        num = Math.floor(Math.random() * multiplier) + 1;
+      } while (num > 15);
+      return num;
+    } else if (multiplier === 100 && this.props.diff === "Hard") {
+      do {
+        num = Math.floor(Math.random() * multiplier) + 1;
+      } while (num > 50);
+      return num;
+    }
+
+    if (this.props.diff === "Medium") {
+      //for addition or subtraction
+      do {
+        num = Math.floor(Math.random() * multiplier) + 1;
+      } while (num > 500);
+      return num;
+    }
+    return Math.floor(Math.random() * multiplier) + 1;
   }
 }
 
